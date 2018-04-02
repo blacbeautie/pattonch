c4518a6eb2d64caf3775242927fe5fefec3d97e5
Patton <pattonch@gmail.com>
2018-04-02 11:17:35 -0700

    First commit

---
+++ LightViewMirror_LED/LightViewMirror_LED.ino
@@ -0,0 +1,36 @@
+#include <font5x7.h>
+#include <LedDisplay.h>
+
+int testled = 13;     // LED on teensy is at pin 13.
+
+
+void setup() {    // put your setup code here, to run once:
+  
+  pinMode(testled, OUTPUT);
+  
+  digitalWrite(testled, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                  // wait for a second
+  digitalWrite(testled, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000); 
+
+  for (int i = 1; i <= 2; i++) {
+    digitalWrite(testled, HIGH);  // turn the LED on (HIGH is the voltage level)
+    delay(100);                  // wait for a second
+    digitalWrite(testled, LOW);   // turn the LED off by making the voltage LOW
+    delay(100);                   // wait for a second
+ }
+ Serial.begin(9600);
+ Serial.println("Setup completed successfully!");
+}
+
+int rgbpanel = 19;
+
+
+void loop() {     // put your main code here, to run repeatedly:
+  
+  pinMode(rgbpanel, OUTPUT);
+
+  digitalWrite(rgbpanel, HIGH);
+  delay (1000);
+   
+}
